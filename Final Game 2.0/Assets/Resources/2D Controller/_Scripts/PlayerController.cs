e3dc7f2f1e32aca67a7fab2e264559b05a6b30ec
SaheedKazeem <saheekazeem@gmail.com>
2022-02-17 14:02:40 +0000

    Added Tilesets

--- Final Game 2.0/Assets/Resources/2D Controller/_Scripts/PlayerController.cs
+++ Final Game 2.0/Assets/Resources/2D Controller/_Scripts/PlayerController.cs
@@ -7,11 +7,7 @@ using Random = UnityEngine.Random;
 namespace MasterController
 {
     /// <summary>
-    /// Hey!
-    /// Tarodev here. I built this controller as there was a severe lack of quality & free 2D controllers out there.
-    /// Right now it only contains movement and jumping, but it should be pretty easy to expand... I may even do it myself
-    /// if there's enough interest. You can play and compete for best times here: https://tarodev.itch.io/
-    /// If you hve any questions or would like to brag about your score, come to discord: https://discord.gg/GqeHHnhHpz
+    /// Thanks to Tarodev for the amazing player controller, Credits:https://tarodev.itch.io/
     /// </summary>
     [RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour, IPlayerController

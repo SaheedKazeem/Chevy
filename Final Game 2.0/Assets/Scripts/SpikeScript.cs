e55be88433b42e2f14be577d6c13c91a92757a46
Saheed ⭐ <saheekazeem@gmail.com>
2022-02-22 13:24:38 +0000

    Unsheath animation and collision on spike
    Still tryna find a way to not mess up with ground collider

---
+++ Final Game 2.0/Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour
+{
+    Animator SpikeAnimator;
+    bool spikeHasBeenPressed;
+    private void Awake()
+    {
+        SpikeAnimator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            spikeHasBeenPressed = true;
+            if(spikeHasBeenPressed == true)
+            {
+                SpikeAnimator.SetTrigger(SpikeActivateKey);
+            }
+            
+        }
+        else spikeHasBeenPressed = false;
+    }
+    private static readonly int SpikeActivateKey = Animator.StringToHash("ActivateWhenStandingOn");
+}

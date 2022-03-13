028a3cf9c025ef109e6e1b947892f675036edf00
SaheedKazeem <saheekazeem@gmail.com>
2022-03-13 16:30:25 +0000

    i messed up

--- Final Game 2.0/Assets/Resources/2D Controller/_Scripts/PlayerExtras.cs
+++ Final Game 2.0/Assets/Resources/2D Controller/_Scripts/PlayerExtras.cs
@@ -1,37 +1,36 @@
 using System;
 using UnityEngine;
 
-namespace MasterController
-{
-    public struct FrameInput
-    {
-        public float X, Y;
-        public bool JumpDown;
-        public bool JumpHeld;
-        public bool DashDown;
-    }
+namespace TarodevController {
+	public struct FrameInput {
+		public float X,Y;
+		public bool JumpDown;
+		public bool JumpHeld;
+		public bool DashDown;
+	}
 
-    public interface IPlayerController
-    {
-         FrameInput Input { get; }
-         Vector3 RawMovement { get; }
-         bool Grounded { get; }
+	public interface IPlayerController {
+		public FrameInput Input { get; }
+		public Vector3 RawMovement { get; }
+		public bool Grounded { get; }
 
-         event Action<bool> OnGroundedChanged;
-         event Action OnJumping, OnDoubleJumping;
-         event Action<bool> OnDashingChanged;
-         event Action<bool> OnCrouchingChanged;
-    }
+		public event Action<bool> OnGroundedChanged;
+		public event Action OnJumping,OnDoubleJumping;
+		public event Action<bool> OnDashingChanged;
+	}
+    
+	public interface IExtendedPlayerController : IPlayerController {
+		public bool DoubleJumpingThisFrame { get; set; }
+		public bool Dashing { get; set; }  
+	}
 
-    public struct RayRange
-    {
-        public RayRange(float x1, float y1, float x2, float y2, Vector2 dir)
-        {
-            Start = new Vector2(x1, y1);
-            End = new Vector2(x2, y2);
-            Dir = dir;
-        }
+	public struct RayRange {
+		public RayRange(float x1, float y1, float x2, float y2, Vector2 dir) {
+			Start = new Vector2(x1, y1);
+			End = new Vector2(x2, y2);
+			Dir = dir;
+		}
 
-        public readonly Vector2 Start, End, Dir;
-    }
+		public readonly Vector2 Start, End, Dir;
+	}
 }

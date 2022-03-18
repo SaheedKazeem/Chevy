a27a94ef035e41ef3c98520457bd3d5022ce8065
SaheedKazeem <saheekazeem@gmail.com>
2022-03-18 14:27:28 +0000

    does it work tho?

---
+++ Final Game 2.0/Assets/Resources/2D Controller/_Scripts/PlayerExtras.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace TarodevController {
+	public struct FrameInput {
+		public float X,Y;
+		public bool JumpDown;
+		public bool JumpHeld;
+		public bool DashDown;
+	}
+
+	public interface IPlayerController {
+		public FrameInput Input { get; }
+		public Vector3 RawMovement { get; }
+		public bool Grounded { get; }
+
+		public event Action<bool> OnGroundedChanged;
+		public event Action OnJumping,OnDoubleJumping;
+		public event Action<bool> OnDashingChanged;
+	}
+    
+	public interface IExtendedPlayerController : IPlayerController {
+		public bool DoubleJumpingThisFrame { get; set; }
+		public bool Dashing { get; set; }  
+	}
+
+	public struct RayRange {
+		public RayRange(float x1, float y1, float x2, float y2, Vector2 dir) {
+			Start = new Vector2(x1, y1);
+			End = new Vector2(x2, y2);
+			Dir = dir;
+		}
+
+		public readonly Vector2 Start, End, Dir;
+	}
+}

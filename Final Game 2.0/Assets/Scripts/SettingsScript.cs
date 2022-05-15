073b3c85a0cdb98142d1dda4877a0090ffd9b38b
SaheedKazeem <saheekazeem@gmail.com>
2022-05-15 16:05:49 +0100

    Adding Volume slider

--- Final Game 2.0/Assets/Scripts/SettingsScript.cs
+++ Final Game 2.0/Assets/Scripts/SettingsScript.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsScript : MonoBehaviour
 {
+    public AudioMixer ReftoAudioMixer;
     // Start is called before the first frame update
-  public void
+  public void SetVolume (float volume)
+  {
+      ReftoAudioMixer.SetFloat("Volume", volume);
+  }
 }
